a697b5f2ab024426e13400e3cfb9f0df4c5860fe
johnpstroud <johnstroud701@gmail.com>
2024-10-28 17:07:49 -0500

    Removed the auto scroll to the top as it was annoying when navigating files

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Has the web page start at the top every time you refresh
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
-
 // Turns on an indicator to scroll down if the user does nothing for 2 seconds
 setTimeout(myFunction, 2000);
 function myFunction() {
