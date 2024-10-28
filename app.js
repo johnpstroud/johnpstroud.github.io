80475a9ad6e791ede0a4d069bf8ba53298d96d69
johnpstroud <johnstroud701@gmail.com>
2024-10-28 17:04:44 -0500

    Added js for smoother usage

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-function MyButton() {
-    return (
-      <button>
-        I'm a button
-      </button>
-    );
-  }
-  
-  export default function MyApp() {
-    return (
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton />
-      </div>
-    );
-  }
-  
+// Has the web page start at the top every time you refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+// Turns on an indicator to scroll down if the user does nothing for 2 seconds
+setTimeout(myFunction, 2000);
+function myFunction() {
+  document.getElementById("indicator").style.visibility = 'visible';
+}
+
+// Hides the scroll indicator if the user scrolls down
+window.addEventListener('scroll',(event) => {
+  document.getElementById("indicator").style.visibility = 'hidden';
+});
