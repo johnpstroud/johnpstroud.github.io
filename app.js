28d0b9ddb9f3fb41e7d9bee17b517a8672389948
johnpstroud <johnstroud701@gmail.com>
2024-10-29 15:20:50 -0500

    Added interactive buttons instead of hyperlinks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Turns on an indicator to scroll down if the user does nothing for 2 seconds
-setTimeout(myFunction, 2000);
-function myFunction() {
+setTimeout(indicator, 2000);
+function indicator() {
   document.getElementById("indicator").style.visibility = 'visible';
 }
 
@@ -8,3 +8,16 @@ function myFunction() {
 window.addEventListener('scroll',(event) => {
   document.getElementById("indicator").style.visibility = 'hidden';
 });
+
+// Each of these make the buttons send to their respective places
+function github() {
+  location.href="https://github.com/johnpstroud?tab=repositories";
+}
+
+function linkedin() {
+  location.href="http://linkedin.com/in/jpstroud";
+}
+
+function gmail() {
+  location.href="mailto:johnstroud701@gmail.com";
+}
